d0b609683a9a9d647756888221c942b119590cb3
krakenlink243 <vquochoang7@gmail.com>
2025-06-06 19:05:14 +0700

    cmt

--- src/main/java/com/se1933g01/steam_clone_backend/service/GoogleDriveService.java
+++ src/main/java/com/se1933g01/steam_clone_backend/service/GoogleDriveService.java
@@ -1,6 +1,5 @@
 package com.se1933g01.steam_clone_backend.service;
 
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -10,7 +9,14 @@ import java.io.IOException;
 import java.util.Collections;
 
 public class GoogleDriveService {
-    private static final String APPLICATION_NAME = "Steam CLone";
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    public static Drive getDriveService() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("credentials.json"))
+                .createScoped(Collections.singleton("https://www.googleapis.com/auth/drive.file"));
+        return new Drive.Builder(new com.google.api.client.http.javanet.NetHttpTransport(),
+                new com.google.api.client.json.gson.GsonFactory(),
+                new HttpCredentialsAdapter(credentials))
+                .setApplicationName("MyApp")
+                .build();
+    }
 }
