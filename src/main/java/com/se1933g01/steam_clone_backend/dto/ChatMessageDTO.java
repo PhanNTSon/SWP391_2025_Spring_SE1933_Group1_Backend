b0cbdc6d6dc1a427343e4579597986ce9cee8068
Phan Son <minikie456@gmail.com>
2025-06-23 09:20:48 +0700

    Featrue: done implementing WebSocket for Chat Feature between 2 users

---
+++ src/main/java/com/se1933g01/steam_clone_backend/dto/ChatMessageDTO.java
@@ -0,0 +1,17 @@
+package com.se1933g01.steam_clone_backend.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessageDTO {
+    private String senderUsername;
+    private String receiverUsername;
+    private String content;
+    private LocalDateTime sentAt;
+}
