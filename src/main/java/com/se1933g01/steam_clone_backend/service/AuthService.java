30360a75e03ebecf07fca49f36516fa04570a3e5
Loc <phangialoc2005@gmail.com>
2025-06-24 08:27:03 +0700

    Fix: Bugs found by SonarQube

--- src/main/java/com/se1933g01/steam_clone_backend/service/AuthService.java
+++ src/main/java/com/se1933g01/steam_clone_backend/service/AuthService.java
@@ -22,7 +22,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
