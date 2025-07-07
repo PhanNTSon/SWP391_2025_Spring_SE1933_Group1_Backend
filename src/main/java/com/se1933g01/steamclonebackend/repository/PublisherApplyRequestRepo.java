cece23946c547bdb1f9370cc408916136bc827de
krakenlink243 <vquochoang7@gmail.com>
2025-07-07 17:07:25 +0700

    fix update publisher

--- src/main/java/com/se1933g01/steamclonebackend/repository/PublisherApplyRequestRepo.java
+++ src/main/java/com/se1933g01/steamclonebackend/repository/PublisherApplyRequestRepo.java
@@ -7,6 +7,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.se1933g01.steamclonebackend.entity.request.PublisherApplyRequest;
+import com.se1933g01.steamclonebackend.entity.request.Request;
+import com.se1933g01.steamclonebackend.entity.user.User;
 
 @Repository
 public interface PublisherApplyRequestRepo extends JpaRepository<PublisherApplyRequest, Long> {
@@ -19,4 +21,8 @@ public interface PublisherApplyRequestRepo extends JpaRepository<PublisherApplyR
     "JOIN r.user u " +
     "WHERE agr.requestId = :requestId")
     String findUserIdByRequestId(@Param("requestId") Long requestId);
+    @Query("SELECT p FROM PublisherApplyRequest p WHERE p.request.user.id = :userId")
+    PublisherApplyRequest findByRequestUserId(@Param("userId") Long userId);
+
+
 }
