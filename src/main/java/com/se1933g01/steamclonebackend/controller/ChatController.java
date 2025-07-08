c14a1cb6e2510b778b34fc80220d053b6297ee9f
Phan Son <minikie456@gmail.com>
2025-07-08 16:36:04 +0700

    Fix: modifying channel to make sure send at correct channel

--- src/main/java/com/se1933g01/steamclonebackend/controller/ChatController.java
+++ src/main/java/com/se1933g01/steamclonebackend/controller/ChatController.java
@@ -37,7 +37,8 @@ public class ChatController {
         msg.setSentAt(LocalDateTime.now());
         communityService.saveMessage(msg, msg.getSenderUsername());
 
-        messagingTemplate.convertAndSendToUser(msg.getReceiverUsername(), "/queue/messages", msg);
+        messagingTemplate.convertAndSendToUser(msg.getReceiverUsername(), "/queue/messages/" + msg.getSenderUsername(),
+                msg);
     }
 
     @SubscribeMapping("/online")
