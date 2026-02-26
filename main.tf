30c7ee635b6d06b4ac522ce7e391df0f7b026a92
krakenlink243 <vquochoang7@gmail.com>
2026-02-26 14:58:09 +0700

    update tf

--- main.tf
+++ main.tf
@@ -2,23 +2,30 @@ provider "google" {
   project     = "ppjg-236308"
   region      = "us-central1"
 }
+module "gce" {
+  source = "terraform-google-modules/container-vm/google"
+  version = "~> 3.0"
+  container = {
+    image = "nginx:latest"
+  }
+}
 terraform {
   backend "gcs" {
     bucket = "terraform-state243"
     prefix = "terraform/state"
   }
 }
-data "google_compute_image" "image" {
-  project = "ubuntu-os-cloud"
-  family = "ubuntu-2404-lts-amd64"
-}
+# data "google_compute_image" "image" {
+#   project = "ubuntu-os-cloud"
+#   family = "ubuntu-2404-lts-amd64"
+# }
 resource "google_compute_instance" "address" {
   name = "centurion"
   machine_type = "e2-micro"
   zone = "us-central1-a"
   boot_disk {
     initialize_params {
-      image = data.google_compute_image.image.name
+      image = module.gce.source_image
       size = 10
       type = "pd-standard"
     }
@@ -31,16 +38,35 @@ resource "google_compute_instance" "address" {
     subnetwork = "private1"
   }
   metadata_startup_script = file("install.sh")
-  
+  metadata = {
+    gce-container-declaration = module.gce.container_declaration
+  }
+  service_account {
+    email = "737111970878-compute@developer.gserviceaccount.com"
+    scopes = [ "cloud-platform" ]
+  }
 }
 resource "google_compute_network" "VPC" {
   name = "private"
   auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "Subnetwork" {
+  depends_on = [ google_compute_network.VPC ]
   name = "private1"
   ip_cidr_range = "10.0.0.0/24"
   region = "us-central1"
   network = "private"
 }
-
+resource "google_compute_firewall" "Firewall" {
+  depends_on = [ google_compute_subnetwork.Subnetwork ]
+  name = "private-firewall"
+  network = "private"
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports = [ "80" ]
+  }
+  source_ranges = [ "0.0.0.0/0" ]
+}
