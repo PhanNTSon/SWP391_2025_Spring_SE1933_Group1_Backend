de83de34841648f6bc56f789fcf0072658105d7d
krakenlink243 <vquochoang7@gmail.com>
2026-03-17 00:37:09 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "google_compute_firewall" "Firewall" {
   }
   allow {
     protocol = "tcp"
-    ports = [ "80", "443","22","31590","30000" ]
+    ports = [ "80", "443","22","31590","30000","31942" ]
   }
   source_ranges = [ "0.0.0.0/0" ]
 }
