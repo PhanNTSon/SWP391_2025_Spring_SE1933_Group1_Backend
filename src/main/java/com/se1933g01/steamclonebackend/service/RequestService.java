a45ad127824274c1f9c5ca9904d845a701fdb359
krakenlink243 <vquochoang7@gmail.com>
2026-04-09 14:32:38 +0700

    disable gcp secret manager

--- src/main/java/com/se1933g01/steamclonebackend/service/RequestService.java
+++ src/main/java/com/se1933g01/steamclonebackend/service/RequestService.java
@@ -149,7 +149,7 @@ public class RequestService {
         game.setGameUrl(addingGameRequest.getGameUrl());
         game.setIconUrl(addingGameRequest.getIconUrl());
         game.setUpdateLog(addingGameRequest.getUpdateLog());
-        gameRepo.save(game); 
+        // gameRepo.save(game); 
 
         List<String> mediaUrls = addingGameRequest.getMediaUrls();
         List<Media> mediaList = mediaUrls.stream()
